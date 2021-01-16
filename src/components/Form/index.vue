60c098a9c8969a555a9190beba2dfb682faaa051
Paulo <pauloalenquer@gmail.com>
2021-01-16 20:35:45 -0300

    first commit

---
+++ src/components/Form/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <FormulateForm @submit="submitHandler" #default="{ isLoading }">
+    <fieldset>
+      <legend>VueForm</legend>
+      <FormulateInput
+        name="name"
+        label="Name"
+        type="text"
+        validation="required"
+      />
+      <FormulateInput
+        name="email"
+        label="E-mail"
+        type="email"
+        validation="required|email"
+        placeholder="test@example.com"
+      />
+      <FormulateInput
+        label="New password"
+        type="password"
+        name="password"
+        validation="required"
+      />
+      <FormulateInput
+        label="Confirm password"
+        type="password"
+        name="password_confirm"
+        validation="required|confirm"
+        validation-name="Password confirmation"
+      />
+      <FormulateInput
+        type="checkbox"
+        label="I accept the terms of service?"
+        name="terms"
+        validation="accepted"
+      />
+      <FormulateInput
+        type="submit"
+        :disabled="isLoading"
+        :label="isLoading ? 'Loading...' : 'Submit'"
+      />
+    </fieldset>
+  </FormulateForm>
+</template>
+
+<script>
+export default {
+  methods: {
+    async submitHandler(data) {
+      await this.$axios.post("/my/api", data);
+      alert(`Thank you, ${data.name}`);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.formulate-form {
+  padding: 2rem;
+  height: 100%;
+  font-size: 1.6rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.formulate-input {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+fieldset {
+  border: 1px solid #ccc;
+  border-radius: 1rem;
+  max-width: 400px;
+  width: 100%;
+  padding: 1.5rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+legend {
+  font-size: 2.5rem;
+  font-weight: bold;
+  text-align: center;
+  padding: 0rem 1rem;
+  color: #41b883;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueFormulate from '@braid/vue-formulate'
+
+import './assets/global.css'
+import './assets/formulate.css'
+
+Vue.use(VueFormulate)
 
 Vue.config.productionTip = false
 
